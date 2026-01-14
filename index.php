bc7be63e9626b81e5f8006bbb12dce0fc97b7226
Minda Nandi <juliennenandi@gmail.com>
2026-01-14 13:56:05 +0400

    insertions de la page de deconnexion

--- index.php
+++ index.php
@@ -1,14 +1,51 @@
+<?php
+session_start();
+include("db.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $CodeEtudiant = $_POST['CodeEtudiant'];
+    $Nom = $_POST['Nom'];
+
+    $stmt = $conn->prepare("SELECT * FROM etudiant WHERE CodeEtudiant = ?");
+    $stmt->execute([$code]);
+    $etudiant = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($etudiant && password_verify($pass, $etudiant['password'])) {
+        $_SESSION['etudiant'] = $etudiant['codeEtudiant'];
+        $_SESSION['Nom'] = $etudiant['Nom'];
+        $_SESSION['Prenom'] = $etudiant['Prenom'];
+        $_SESSION['Adresse'] = $etudiant['Adresse'];
+        $_SESSION['Classe'] = $etudiant['Classe'];
+        header("Location: tableau.php");
+        exit();
+    } else {
+        $error = "Code étudiant ou mot de passe incorrect";
+    }
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bibliotheque en ligne</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-</head>
+<html>
+<head><title>Connexion</title></head>
 <body>
-   <nav>
-    
-   </nav>
+<h2>Connexion Étudiant</h2>
+<form method="POST">
+    <label for="">CodeEtudiant</label>
+  <input type="text" name="codeEtudiant"><br>
+
+    <label for="">Nom</label>
+  <input type="text" name="Nom"><br>
+
+    <label for="">Prenom</label>
+   <input type="text" name="Prenom"><br>
+
+    <label for="">Adesse</label>
+  <input type="text" name="Adresse"><br>
+
+    <label for="">Classe</label>
+  <input type="text" name="Classe"><br>
+  <button type="submit">Se connecter</button>
+</form>
+<a href="register.php">Créer un compte</a>
+<?php if(isset($error)) echo "<p style='color:red'>$error</p>"; ?>
 </body>
 </html>
