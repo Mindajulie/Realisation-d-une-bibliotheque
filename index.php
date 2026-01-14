c2e68038d3208d886ce82f139a71e72549ac96fd
Minda Nandi <juliennenandi@gmail.com>
2026-01-14 11:12:47 +0400

    creation de la page d'accueil

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bibliotheque en ligne</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-   bonjour 
+   <nav>
+    
+   </nav>
 </body>
 </html>
